2aab67fb4a28b3bce59ea43588da14d07d93efca
Marcin Puc <marcin.e.puc@gmail.com>
2020-04-11 00:11:39 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+mod list;
+
+pub use list::{Cons, List};
+
+#[cfg(test)]
+mod tests {
+    use crate::List;
+    use crate::Cons;
+
+    #[test]
+    fn new_list_is_empty() {
+        let l: List<()> = List::new();
+
+        assert!(l.is_empty());
+    }
+
+    #[test]
+    fn empty_list_is_zero_len() {
+        let l: List<()> = List::new();
+
+        assert_eq!(l.len(), 0);
+    }
+
+    #[test]
+    fn list_len_works_correctly() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+        l.push(2);
+        l.push(3);
+
+        assert_eq!(l.len(), 3);
+    }
+
+    #[test]
+    fn items_are_popped_in_lifo_order() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+        l.push(2);
+        l.push(3);
+
+        assert_eq!(l.pop(), Some(3));
+        assert_eq!(l.pop(), Some(2));
+        assert_eq!(l.pop(), Some(1));
+    }
+
+    #[test]
+    fn list_can_be_consed() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+
+        assert!(matches!(l.cons(), Cons::Cons(1, _)));
+    }
+
+    #[test]
+    fn tail_can_be_consed() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+        l.push(2);
+        l.push(3);
+
+        let tail = l.cons().tail().map(|list| list.into_iter().collect::<Vec<i32>>());
+
+        assert_eq!(tail, Some(vec![2, 1]));
+    }
+
+    #[test]
+    fn no_tail_when_consing_one_element_list() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+
+        assert!(matches!(l.cons().tail(), None));
+    }
+
+    #[test]
+    fn list_can_be_iterated() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+        l.push(2);
+        l.push(3);
+
+        let collected: Vec<i32> = l.iter().copied().collect();
+
+        assert_eq!(collected, vec![3, 2, 1]);
+    }
+
+    #[test]
+    fn list_can_be_mutably_iterated() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+        l.push(2);
+        l.push(3);
+
+        l.iter_mut().for_each(|el| *el *= 2);
+
+        let collected: Vec<i32> = l.iter().copied().collect();
+
+        assert_eq!(collected, vec![6, 4, 2]);
+    }
+
+    #[test]
+    fn list_can_be_transformed_into_iterator() {
+        let mut l: List<i32> = List::new();
+
+        l.push(1);
+        l.push(2);
+        l.push(3);
+
+        let collected: Vec<i32> = l.into_iter().collect();
+
+        assert_eq!(collected, vec![3, 2, 1]);
+    }
+}
---
+++ src/list.rs
@@ -0,0 +1,171 @@
+use std::iter::FusedIterator;
+
+struct Node<T> {
+    value: T,
+    next: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+pub enum Cons<T> {
+    Cons(T, List<T>),
+    Nil,
+}
+
+pub struct Iter<'a, T> {
+    current_node: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T> {
+    current_node: Option<&'a mut Node<T>>,
+}
+
+pub struct IntoIter<T> {
+    list: List<T>,
+}
+
+impl<T> Cons<T> {
+    pub fn is_nil(&self) -> bool {
+        matches!(self, Cons::Nil)
+    }
+
+    pub fn head(self) -> Option<T> {
+        match self {
+            Cons::Cons(head, _) => Some(head),
+            _ => None,
+        }
+    }
+
+    pub fn tail(self) -> Option<List<T>> {
+        match self {
+            Cons::Cons(_, tail) if !tail.is_empty() => Some(tail),
+            _ => None,
+        }
+    }
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        matches!(self.head, None)
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Node {
+            value: elem,
+            next: self.head.take(),
+        };
+
+        self.head = Some(Box::new(new_node));
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.pop_node().map(|node| node.value)
+    }
+
+    pub fn len(&self) -> usize {
+        self.iter().count()
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.value)
+    }
+
+    pub fn cons(mut self) -> Cons<T> {
+        match self.head.take() {
+            Some(node) => {
+                let tail = List { head: node.next };
+                Cons::Cons(node.value, tail)
+            }
+            _ => Cons::Nil,
+        }
+    }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter::from(self)
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut::from(self)
+    }
+
+    fn pop_node(&mut self) -> Link<T> {
+        self.head.take().map(|mut node| {
+            self.head = node.next.take();
+            node
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop_node() {}
+    }
+}
+
+impl<T> IntoIterator for List<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoIter { list: self }
+    }
+}
+
+impl<'a, T> Iter<'a, T> {
+    pub fn from(list: &'a List<T>) -> Self {
+        Iter {
+            current_node: list.head.as_ref().map(|node| &**node),
+        }
+    }
+}
+
+impl<'a, T> IterMut<'a, T> {
+    pub fn from(list: &'a mut List<T>) -> Self {
+        IterMut {
+            current_node: list.head.as_mut().map(|node| &mut **node),
+        }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current_node.map(|node| {
+            self.current_node = node.next.as_ref().map(|node| &**node);
+
+            &node.value
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.current_node.take().map(|node| {
+            self.current_node = node.next.as_mut().map(|node| &mut **node);
+
+            &mut node.value
+        })
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.pop()
+    }
+}
+
+impl<'a, T> FusedIterator for Iter<'a, T> {}
+impl<T> FusedIterator for IntoIter<T> {}
